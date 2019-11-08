ca201ceb77f944a06401a9f50b8a6f33dfa9aa5f
fanxiao168 <fanxiao168@126.com>
2019-11-08 17:16:19 +0800

    Flutter中通过TabController定义顶部tab切换 介绍生命周期函数

---
+++ lib/15.Flutter中通过TabController定义顶部tab切换 介绍生命周期函数/routes/routes.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import '../pages/Tabs.dart';
+import '../pages/Form.dart';
+import '../pages/Search.dart';
+import '../pages/Product.dart';
+import '../pages/ProductInfo.dart';
+import '../pages/user/Login.dart';
+import '../pages/user/RegisterFirst.dart';
+import '../pages/user/RegisterSecond.dart';
+import '../pages/user/RegisterThird.dart';
+import '../pages/AppBarDemo.dart';
+import '../pages/TabBarController.dart';
+
+//配置路由
+final routes = {
+    "/":(context,{arguments})=>Tabs(),
+    "/form":(context,{arguments})=>FormPage(arguments:arguments),
+    "/product":(context,{arguments})=>ProductPage(),
+    "/productinfo":(context,{arguments})=>ProductInfoPage(arguments: arguments),
+    "/search":(context,{arguments})=>SearchPage(arguments:arguments),
+    "/login":(context)=>LoginPage(),
+    "/registerFirst":(context)=>RegisterFirstPage(),
+    "/registerSecond":(context)=>RegisterSecondPage(),
+    "/registerThird":(context)=>RegisterThirdPage(),
+    "/appBarDemo":(context)=>AppBarDemoPage(),
+    "/tabBarController":(context)=>TabBarControllerPage(),
+};
+
+//固定写法
+  var onGenerateRoute = (RouteSettings settings){
+        //统一处理
+        final String name = settings.name;
+        final Function pageContentBuilder = routes[name];
+        if(pageContentBuilder != null) {
+          print(name);
+          if(settings.arguments != null){
+            final Route route = MaterialPageRoute(
+              builder: (context)=>pageContentBuilder(context,arguments:settings.arguments));
+              return route;
+          }else{
+            final Route route = MaterialPageRoute(
+              builder: (context) => pageContentBuilder(context));
+            return route;
+          }
+        }else{
+          return null;
+        }
+  };
